4223e8eca8c25ff06affc54fa7a11f839ca72509
TanuragBishnoi <86725157+TanuragBishnoi@users.noreply.github.com>
2021-08-23 17:40:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var runner,image
+var path,pathimage
+function preload(){
+  //pre-load images
+image=loadAnimation("Runner-1.png","Runner-2.png")
+pathimage=loadImage("path.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+runner=createSprite(100,100,100,100)
+runner.addAnimation("runner",image)
+path=createSprite(100,100,200,200)
+path.addImage(pathimage)
+}
+
+function draw() {
+  background(0);
+if(path.y > 400){
+  path.y = height/2
+}
+// Moving background
+path=createSprite(200,200)
+path.addImage(pathing);
+path.velocityY = 4;
+path.scale=1.2;
+//code to reset the background
+if(path.y > 400 ){
+  path.y = height/2;
+}
+drawSprites()
+}
